62dff14ecabce7c1ea2c01c1f994254a5b37b59b
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-05-05 23:26:05 -0600

    Create setup_windows.ps1

---
+++ setup_windows.ps1
@@ -0,0 +1,4 @@
+# Z3R0-T3RM Windows Setup (Ejecutar como Administrator)
+Write-Host "[+] Instalando Windows Terminal + ZSH en WSL2..." -ForegroundColor Cyan
+wsl --install -d Kali
+Write-Host "[✅] Después de reiniciar, dentro de WSL ejecuta: bash installer.sh" -ForegroundColor Green
