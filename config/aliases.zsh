d5df5252dcbc10b71883648ed977222cf42043cd
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-05-05 23:27:12 -0600

    Create aliases.zsh

---
+++ config/aliases.zsh
@@ -0,0 +1,3 @@
+alias kali-mode='export PS1="\[\033[31m\]┌─[\[\033[32m\]\u@\[\033[36m\]\h\[\033[37m\]:\[\033[33m\]\w\[\033[31m\]]\n└─[\[\033[31m\]\\$\[\033[0m\]\] "'
+alias priv-up='sudo su -'
+alias root-me='sudo -i'
